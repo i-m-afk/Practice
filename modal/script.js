1566e8070b1b6ee7e348ffcdbb752f7cd67ae23c
i-m-afk <rishavkumar208@gmail.com>
2023-06-19 18:11:09 +0530

    update script
    redundant code converted into modules

--- modal/script.js
+++ modal/script.js
@@ -18,13 +18,12 @@ openBtn.addEventListener("click", () => {
 
 // TODO: 4. Create a click event listener for the close-modal-btn that removes the class "open" from the modal
 // BONUS: Also remove the class "open" from the overlay
-closeBtn.addEventListener("click", () => {
-  modal.classList.remove("open");
-  overlay.classList.remove("open");
-});
+closeBtn.addEventListener("click", closeModal);
 
 // BONUS: Add a click event listener to the overlay that removes the class "open" from the modal and the overlay
-overlay.addEventListener("click", () => {
+overlay.addEventListener("click", closeModal);
+
+function closeModal() {
   modal.classList.remove("open");
   overlay.classList.remove("open");
-});
+}
